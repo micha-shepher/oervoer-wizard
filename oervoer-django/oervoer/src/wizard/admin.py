1e7c1be7a70c75cf9ffeb836634014c0ae3be9a2
micha-shepher <micha_shepher@yahoo.com>
2015-08-09 15:40:28 +0200

    added pet search filter

--- oervoer-django/oervoer/src/wizard/admin.py
+++ oervoer-django/oervoer/src/wizard/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from wizard.models import Owner, Pet, Taste, Globals, Package, Ras, Order, MeatType, prefers, Donts, Product, Delivery, PickList
 # Register your models here.
 
-admin.site.register((Owner, Pet, Taste, Globals, Package, Ras, MeatType, prefers, Donts, Delivery))
+admin.site.register((Owner, Taste, Globals, Package, Ras, MeatType, prefers, Donts, Delivery))
 
 class PickListAdmin(admin.ModelAdmin):
 
@@ -25,4 +25,11 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ['vlees__meat_type', 'smaak__taste']
     
 admin.site.register(Product, ProductAdmin)
+
+class PetAdmin(admin.ModelAdmin):
+    fields = ['name', 'weight', 'ras', 'owner', 'factor', 'profile']
+    list_filter = ['name', 'owner']
+    search_fields = ['name', 'owner__name']
+    
+admin.site.register(Pet, PetAdmin)
 
